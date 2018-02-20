edfe8453774a1eed61575f2e1df5f049850f5667
Dervent Wright <dow4964@rit.edu>
2018-02-19 21:01:37 -0500

    Fixed JS issue. Other changes are related to local NodeJS files.

--- js/TedTalkSearch.js
+++ js/TedTalkSearch.js
@@ -8,6 +8,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('../www'));
 app.use(express.static('../css'));
+app.use(express.static('../js'));
 //app.use(express.static('../SiteFiles'));
 
 app.listen(3000,function(){
