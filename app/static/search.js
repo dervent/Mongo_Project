bac81c5fc6db8fff34086bbcaee668567c570b22
Gavin Drabik <grd2747@g.rit.edu>
2018-03-31 12:29:06 -0400

    ajax conversion. radio buttons. back to top button
    still some work to be done with the ajax calls

--- app/static/search.js
+++ app/static/search.js
@@ -2,32 +2,74 @@
  * Client-Side JavaScript
  */
 
-function searchForDocs() {
-    // Get text from search bar
-    var searchText = document.getElementById("searchBar").value.split(/\s+/);
+ function searchForDocs() {
+     // Get text from search bar
+     var searchText = document.getElementById("searchBar").value.split(/\s+/);
 
-    // Form URL query
-    var url = 'http://127.0.0.1:5000/search?text=';
-    for(var i in searchText) {
-        url += "+" + searchText[i];
+     // Form URL query
+     var qurl = 'http://127.0.0.1:5000/search?text=';
+     for(var i in searchText) {
+         qurl += "+" + searchText[i];
+     }
+     // Future URL example to take in filter and sort parameters
+     // http://127.0.0.1:5000/search?text=test+something&filter=description&sort=duration
+
+     $.ajax(
+     {
+      url: qurl,
+      type:"GET",
+      async:true,
+      success:function(result){
+          console.log("jquery is here");
+          $('#results')[0].innerHTML = result;
+      }
+     }
+     );
+     // Make AJAX call to server
+     // var xhttp = new XMLHttpRequest();
+     // xhttp.onreadystatechange = function () {
+     //     if(this.readyState == 4 && this.status == 200) {
+     //         console.log("hereeeee");
+     //         $('#results')[0].innerHTML =
+     //         this.response;
+     //     }
+     // };
+     // xhttp.open("GET", url, true);
+     // xhttp.send();
+ }
+
+
+function openModal(eleID){
+    //ajax GET request information for that ele ID
+    //modal inner html = this.response
+    //modal.show
+    var qurl = 'http://127.0.0.1:5000/search?id='+eleID;
+    $.ajax(
+    {
+     url: qurl,
+     type:"GET",
+     async:true,
+     success:function(result){
+         //the result will be a fully made modal (dialog tag)
+         //get the results div and append the dialog tag
+         $('#results')[0].append(result);
+         //store dialog tag in a variable
+         dialogEle = $('dialog')[0];
+         //dialogEle.show()
+         //THERE NEEDS TO BE A BUTTON IN THE DIALOAG that will call closeModal() function
+         console.log("jquery is here");
+     }
     }
-    // Future URL example to take in filter and sort parameters
-    // http://127.0.0.1:5000/search?text=test+something&filter=description&sort=duration
+    );
 
-    // Make AJAX call to server
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if(this.readyState == 4 && this.status == 200) {
-            document.getElementById('results').innerHTML =
-            this.response;
-        }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
+    console.log(eleID);
 }
+function closeModal(){
+    //get the dialog tag from the DOM
+    //this could be done inline....but thats kinda gross
+    dialogEle = $('dialog')[0];
+    //dialog.close()
 
-function openDetailsModal(docId) {
-    //ajax request to get information for that doc ID
 }
 function addComment() {
     // When the user adds comment, make a POST request
@@ -36,4 +78,6 @@ function addComment() {
 function goBackToTop() {
     // A "Back to Top" button will be displayed on the homepage.
     // When the user scrolls far down and clicks on the button, they should go to the top of the page.
+    $(window).scrollTop(0);
+    console.log("back to top");
 }
