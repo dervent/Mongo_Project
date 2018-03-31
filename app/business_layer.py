6124883cb42f3312df12d84a8ae8ea7482f7b1cc
Dervent Wright <dow4964@rit.edu>
2018-03-31 11:42:35 -0400

    Refactoring backend functions and template for rendering info

--- app/business_layer.py
+++ app/business_layer.py
@@ -7,72 +7,34 @@ class BusinessLayer:
     """
 
     def __init__(self):
-        """
-        Initialize attributes
-        """
         self.dl = data_layer.DataLayer()
         self.documents_list = []
 
     def format_search_text(self, search_text):
-        """
-        Format the search string with RegEx patterns
-        :param search_text: user-specified text
-        :return: search string with RegEx patterns
-        """
         search_text = '\\b' + '.*\\b'.join(search_text.split()) + ".*"
         return search_text.strip()
 
-    def search_by_default(self, search_text):
-        """
-        Perform default search when no filter or sort is specified.
-        :param search_text: user-specified text
-        :return: list of matching documents
-        """
+    def get_names_by_default(self, search_text):
         search_text = self.format_search_text(search_text)
-        cursor = self.dl.search_by_default(search_text)
+        cursor = self.dl.get_names_by_default(search_text)
         for document in cursor:
             # convert duration field to minutes and seconds
             self.documents_list.append(document)
         return self.documents_list
 
-    def search_by_filter(self, search_text, field_filter):
-        """
-        Perform search by filter option
-        :param search_text: user-specified text
-        :param field_filter: user-selected filter option
-        :return: TBD
-        """
+    def get_names_by_filter(self, search_text, field_filter):
         search_text = self.format_search_text(search_text)
         field_filter = field_filter.strip()
 
-    def search_by_sort(self, search_text, sort_field):
-        """
-        Perform search by sort option
-        :param search_text: user-specified text
-        :param sort_field: user-selected sort option
-        :return: TBD
-        """
+    def get_names_by_sort(self, search_text, sort_field):
         search_text = self.format_search_text(search_text)
         sort_field = sort_field.strip()
 
-    def search_by_filter_sort(self, search_text, field_filter, sort_field):
-        """
-        Perform search by filter and sort options
-        :param search_text: user-specified text
-        :param field_filter: user-selected filter option
-        :param sort_field: user-selected sort option
-        :return: TBD
-        """
+    def get_names_by_filter_sort(self, search_text, field_filter, sort_field):
         search_text = self.format_search_text(search_text)
         field_filter = field_filter.strip()
         sort_field = sort_field.strip()
 
     def add_comment(self, object_id, comment):
-        """
-        Add comment to specific result (document)
-        :param object_id: hidden object_id of document
-        :param comment: user-specified comment strinf
-        :return: Response object with status code of 201 (Created) or 500 (Internal Server Error)
-        """
         comment = comment.strip()
         return self.dl.add_comment(object_id, comment)
--- app/data_layer.py
+++ app/data_layer.py
@@ -11,71 +11,32 @@ class DataLayer:
     """
 
     def __init__(self):
-        """
-        Initialize local connection to TedTalks database and Talks collection
-        """
         self.client = MongoClient('localhost', 27017)
         self.database = self.client.TedTalks
         self.collection = self.database.Talks
-        self.excluded_fields = {'main_speaker': 0, 'ratings': 0, 'speaker_occupation': 0, 'title': 0}
+        self.excluded_fields = {'main_speaker': 0, 'ratings': 0, 'speaker_occupation': 0, 'title': 0, '_id': 0}
 
-    def search_by_default(self, search_text):
-        """
-        When a filter (field) or sort is not specified, query for text in the default field, title
-        :param search_text: user-specified text
-        :return: cursor object containing all matching documents
-        """
-        return self.collection.find({'name': {'$regex': search_text, '$options': 'i'}},
-                                    self.excluded_fields)
+    def get_names_by_default(self, search_text):
+        return self.collection.find({'name': {'$regex': search_text, '$options': 'i'}}, {'name': 1})
 
-    def search_by_filter(self, search_text, field_filter):
-        """
-        Query for the text in the specified filter (field)
-        :param search_text: user-specified text
-        :param field_filter: user-selected field
-        :return: cursor object containing all matching documents
-        """
+    def get_names_by_filter(self, search_text, field_filter):
         return self.collection.find({field_filter: {'$regex': search_text, '$options': 'i'}},
                                     self.excluded_fields)
 
-    def search_by_sort(self, search_text, sort_field):
-        """
-        Query for the text in the default field, title, then sort in ascending order
-        by the sort option specified
-        :param search_text: user-specified text
-        :param sort_field: user-selected sort value
-        :return: cursor object containing all matching documents
-        """
+    def get_names_by_sort(self, search_text, sort_field):
         return self.collection.find({'name': {'$regex': search_text, '$options': 'i'}},
                                     self.excluded_fields).sort(sort_field, ASCENDING)
 
-    def search_by_filter_sort(self, search_text, field_filter, sort_field):
-        """
-        Query for the for the text in the specified filter (field), then sort in ascending order
-        by the sort option specified
-        :param search_text: user-specified text
-        :param field_filter: user-selected field
-        :param sort_field: user-selected sort value
-        :return: cursor object containing all matching documents
-        """
+    def get_names_by_filter_sort(self, search_text, field_filter, sort_field):
         return self.collection.find({field_filter: {'$regex': search_text, '$options': 'i'}},
                                     self.excluded_fields).sort(sort_field, ASCENDING)
+
+    def get_details(self, object_id):
+        return self.collection.find({'_id': ObjectId(object_id)}, {self.excluded_fields})
 
     def add_comment(self, object_id, comment):
-        """
-        Add the comment the user entered into the document specified by the BSON ObjectId
-        :param object_id: BSON OBjectId()
-        :param comment: user-specified textual comment
-        :return: Response object with status code of 201 (Created) or 500 (Internal Server Error)
-        """
         update = self.collection.update_one({'_id': ObjectId(object_id)}, {'$push' : {'comments': comment}})
         if update.matched_count == 1:
             return flask.Response(status=201)
         else:
             return flask.Response(status=500)
-
-    def get_image(self, object_id):
-        """
-        TBD
-        """
-        self.collection = None
